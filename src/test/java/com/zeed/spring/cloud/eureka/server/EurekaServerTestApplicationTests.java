6852b3e1fe59ee6d1cf7614fc17d9c4efc335605
zeedt <yusufsaheedtaiwo@gmail.com>
2020-10-07 07:57:15 +0100

    first commit

---
+++ src/test/java/com/zeed/spring/cloud/eureka/server/EurekaServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeed.spring.cloud.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
